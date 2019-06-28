629c75158ac8d8c7f5ed4aa2c40b5f1ec17b2c79
Leocaliban <leocaliban@gmail.com>
2019-06-28 18:32:11 -0300

    Implementa a validação de campos vazios no formulário

--- C_03_elvis_store/source/php/sendemail.php
+++ C_03_elvis_store/source/php/sendemail.php
@@ -13,33 +13,46 @@
 
     <?php
 
-    $email = $_POST['nazzawd@gmail.com'];
+    $email = 'nazzawd@gmail.com';
     $assunto = $_POST['assunto'];
     $mensagem = $_POST['mensagem'];
 
-    $dbc = mysqli_connect('localhost', 'root', 'root', 'php_elvis_store')
-        or die('Erro de conexão com MySQL server.');
-
-    $query =
-        "SELECT * FROM email_list";
-
-    $result = mysqli_query($dbc, $query)
-        or die('Erro na consulta do banco de dados');
-
-    while ($row = mysqli_fetch_array($result)) {
-
-        $primeiro_nome = $row['primeiro_nome'];
-        $sobrenome = $row['sobrenome'];
-
-        $mensagem_email = "Olá, $primeiro_nome $sobrenome, \n $mensagem";
-
-        $email_destinatario = $row['email'];
-
-        mail($email_destinatario, $assunto, $mensagem_email, 'From: ' . 'Elvis Store' . " <" . $email . "> ");
-
-        echo 'E-mail enviado com sucesso para: ' . $email_destinatario . '<br/ >';
+    if (empty($assunto) && empty($mensagem)) {
+        echo 'Você esqueceu de informar o assunto e a mensagem do e-mail <br />';
     }
-    mysqli_close($dbc);
+
+    if (empty($assunto) && (!empty($mensagem))) {
+        echo 'Você esqueceu de informar o assunto do e-mail <br />';
+    }
+
+    if ((!empty($assunto)) && empty($mensagem)) {
+        echo 'Você esqueceu de informar a mensagem do e-mail <br />';
+    }
+
+    if ((!empty($assunto)) && (!empty($mensagem))) {
+        $dbc = mysqli_connect('localhost', 'root', 'root', 'php_elvis_store')
+            or die('Erro de conexão com MySQL server.');
+
+        $query = "SELECT * FROM email_list";
+
+        $result = mysqli_query($dbc, $query)
+            or die('Erro na consulta do banco de dados');
+
+        while ($row = mysqli_fetch_array($result)) {
+
+            $primeiro_nome = $row['primeiro_nome'];
+            $sobrenome = $row['sobrenome'];
+
+            $mensagem_email = "Olá, $primeiro_nome $sobrenome, \n $mensagem";
+
+            $email_destinatario = $row['email'];
+
+            mail($email_destinatario, $assunto, $mensagem_email, 'From: ' . 'Elvis Store' . " <" . $email . "> ");
+
+            echo 'E-mail enviado com sucesso para: ' . $email_destinatario . '<br/ >';
+        }
+        mysqli_close($dbc);
+    }
 
     ?>
 

524428a8b852bfd9fd9c76391b676525f209138d
Leocaliban <leocaliban@gmail.com>
2019-07-10 03:06:53 -0300

    Implementa a exclusão de registros

---
+++ C_05_guitar_wars/source/php/admin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Guitar Wars</title>
+    <meta name="description" content="Inscrever-se na lista de e-mails" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.min.css" />
+</head>
+
+<body>
+    <h2>Guitar Wars - Painel de controle</h2>
+    <p>Abaixo estão todos os participantes do ranking. Use essa página pare remover resgistros.</p>
+    <hr />
+
+
+    <?php
+    require_once('constants/app-vars.php');
+    require_once('constants/connection-vars.php');
+
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+        or die('Erro de conexão com MySQL server.');
+
+    $query = "SELECT * FROM guitarwars ORDER BY pontuacao DESC, data ASC";
+
+    $data = mysqli_query($dbc, $query);
+
+    echo '<table>';
+    while ($row = mysqli_fetch_array($data)) {
+        echo '<tr><td><strong>' . $row['nome'] . '</strong> </td>';
+        echo '<td>' . $row['data'] . '</td>';
+        echo '<td>' . $row['pontuacao'] . '</td>';
+
+        echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;data=' . $row['data'] . '&amp;nome=' . $row['nome'] . '&amp;pontuacao=' . $row['pontuacao'] . '&amp;imagem=' . $row['imagem'] . '">Remover</a></td></tr>';
+    }
+    echo '</table>';
+
+    mysqli_close($dbc);
+    ?>
+</body>
+
+</html>
---
+++ C_05_guitar_wars/source/php/removescore.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Guitar Wars</title>
+    <meta name="description" content="Inscrever-se na lista de e-mails" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.min.css" />
+</head>
+
+<body>
+    <h2>Guitar Wars - Confirmar exclusão</h2>
+    <hr />
+
+    <?php
+    require_once('constants/app-vars.php');
+    require_once('constants/connection-vars.php');
+
+    if (isset($_GET['id']) && isset($_GET['data']) && isset($_GET['nome']) && isset($_GET['pontuacao']) && isset($_GET['imagem'])) {
+
+        $id = $_GET['id'];
+        $nome = $_GET['nome'];
+        $pontuacao = $_GET['pontuacao'];
+        $data = $_GET['data'];
+        $imagem = $_GET['imagem'];
+    } else if (isset($_POST['id']) && isset($_POST['nome']) && isset($_POST['pontuacao'])) {
+        $id = $_POST['id'];
+        $nome = $_POST['nome'];
+        $pontuacao = $_POST['pontuacao'];
+    } else {
+        echo '<p class="error">Desculpe, nenhuma pontuação para remover.</p>';
+    }
+
+    // Formulário submetido
+    if (isset($_POST['submit'])) {
+        if ($_POST['confirm'] == 'Sim') {
+            // Deletar a imagem do servidor
+            @unlink(MY_UPLOADPATH . $imagem);
+
+            $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+                or die('Erro de conexão com MySQL server.');
+
+            $query = "DELETE FROM guitarwars WHERE id = $id LIMIT 1";
+
+            mysqli_query($dbc, $query);
+            mysqli_close($dbc);
+
+            // Confirmação da exclusão
+            echo '<p>A pontuação de ' . $nome . ' com ' . $pontuacao . ' pontos, foi removida com sucesso.';
+        } else {
+            echo '<p class="error">Ocorreu um erro e a pontuação não foi removida.</p>';
+        }
+    } else if (isset($id) && isset($nome) && isset($data) && isset($pontuacao)) {
+        echo '<p>Você tem certeza que quer deletar esse registro?</p>';
+        echo '<p><strong>Nome: </strong>' . $nome . '<br /><strong>Data: </strong>' . $data .
+            '<br /><strong>Pontuação: </strong>' . $pontuacao . '</p>';
+        echo '<form method="post" action="removescore.php">';
+        echo '<input type="radio" name="confirm" value="Sim" /> Sim ';
+        echo '<input type="radio" name="confirm" value="Não" checked="checked" /> Não <br /><br />';
+        echo '<input type="submit" value="Deletar" name="submit" />';
+
+        echo '<input type="hidden" name="id" value="' . $id . '" />';
+        echo '<input type="hidden" name="nome" value="' . $nome . '" />';
+        echo '<input type="hidden" name="pontuacao" value="' . $pontuacao . '" />';
+        echo '</form>';
+    }
+
+    echo '<p><a href="admin.php">&lt;&lt; Voltar para  o Painel de controle</a></p>';
+
+    ?>
+
+
+
+</body>
+
+</html>

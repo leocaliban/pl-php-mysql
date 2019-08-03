2d2913dbe80fe95a6b8c497f32f579bf89c5f21c
Leocaliban <leocaliban@gmail.com>
2019-08-03 02:23:01 -0300

    Aplica estilos na visualização do perfil do usuário

--- C_07_mismatch/source/php/viewprofile.php
+++ C_07_mismatch/source/php/viewprofile.php
@@ -13,77 +13,79 @@ require_once('login.php');
 </head>
 
 <body>
-  <h2>Mismatch - Perfil</h2>
+  <nav>
+    <a class="menu" href="index.php">Início</a>
+    <a class="menu" href="signup.php">Criar conta</a>
+    <a class="menu" href="editprofile.php">Editar Perfil</a>
+  </nav>
 
-  <?php
-  require_once('constants/app-vars.php');
-  require_once('constants/connection-vars.php');
+  <section class="view-section">
+    <h2 class="view-title">Perfil do usuário</h2>
 
-  // Conexão com BD
-  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-    or die('Erro de conexão com MySQL server.');
+    <?php
+    require_once('constants/app-vars.php');
+    require_once('constants/connection-vars.php');
 
-  // Recuperar dados do BD
-  if (!isset($_GET['id'])) {
-    $query = "SELECT username, nome, sobrenome, genero, nascimento, cidade, estado, imagem FROM usuario WHERE id = '$id'";
-  } else {
-    $query = "SELECT username, nome, sobrenome, genero, nascimento, cidade, estado, imagem FROM usuario WHERE id = '" . $_GET['id'] . "'";
-  }
+    // Conexão com BD
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+      or die('Erro de conexão com MySQL server.');
 
-  $data = mysqli_query($dbc, $query);
-
-  if (mysqli_num_rows($data) == 1) {
-    $row = mysqli_fetch_array($data);
-    echo '<table>';
-    if (!empty($row['username'])) {
-      echo '<tr><td class="label">Nome do usuário:</td><td>' . $row['username'] . '</td></tr>';
+    // Recuperar dados do BD
+    if (!isset($_GET['id'])) {
+      $query = "SELECT username, nome, sobrenome, genero, nascimento, cidade, estado, imagem FROM usuario WHERE id = '$id'";
+    } else {
+      $query = "SELECT username, nome, sobrenome, genero, nascimento, cidade, estado, imagem FROM usuario WHERE id = '" . $_GET['id'] . "'";
     }
-    if (!empty($row['nome'])) {
-      echo '<tr><td class="label">Nome:</td><td>' . $row['nome'] . '</td></tr>';
-    }
-    if (!empty($row['sobrenome'])) {
-      echo '<tr><td class="label">Sobrenome:</td><td>' . $row['sobrenome'] . '</td></tr>';
-    }
-    if (!empty($row['genero'])) {
-      echo '<tr><td class="label">Gênero:</td><td>';
-      if ($row['genero'] == 'M') {
-        echo 'Masculino';
-      } else if ($row['genero'] == 'F') {
-        echo 'Feminino';
-      } else {
-        echo '?';
+
+    $data = mysqli_query($dbc, $query);
+
+    if (mysqli_num_rows($data) == 1) {
+      $row = mysqli_fetch_array($data);
+
+      echo '<img class="view-image" src="' . MM_UPLOADPATH . $row['imagem'] . '" alt="Foto de perfil" />';
+
+      echo '<table>';
+      if (!empty($row['username'])) {
+        echo '<tr><td class="label">Nome do usuário:</td><td>' . $row['username'] . '</td></tr>';
       }
-      echo '</td></tr>';
-    }
-    if (!empty($row['nascimento'])) {
-      if (!isset($_GET['id']) || ($id == $_GET['id'])) {
-        // Show the user their own birthdate
-        echo '<tr><td class="label">Data de nascimento:</td><td>' . $row['nascimento'] . '</td></tr>';
-      } else {
-        // Show only the birth year for everyone else
-        list($year, $month, $day) = explode('-', $row['nascimento']);
-        echo '<tr><td class="label">Ano de nascimento:</td><td>' . $year . '</td></tr>';
+      if (!empty($row['nome'])) {
+        echo '<tr><td class="label">Nome:</td><td>' . $row['nome'] . '</td></tr>';
       }
+      if (!empty($row['sobrenome'])) {
+        echo '<tr><td class="label">Sobrenome:</td><td>' . $row['sobrenome'] . '</td></tr>';
+      }
+      if (!empty($row['genero'])) {
+        echo '<tr><td class="label">Gênero:</td><td>';
+        if ($row['genero'] == 'M') {
+          echo 'Masculino';
+        } else if ($row['genero'] == 'F') {
+          echo 'Feminino';
+        } else {
+          echo '?';
+        }
+        echo '</td></tr>';
+      }
+      if (!empty($row['nascimento'])) {
+        if (!isset($_GET['id']) || ($id == $_GET['id'])) {
+          // Show the user their own birthdate
+          echo '<tr><td class="label">Data de nascimento:</td><td>' . $row['nascimento'] . '</td></tr>';
+        } else {
+          // Show only the birth year for everyone else
+          list($year, $month, $day) = explode('-', $row['nascimento']);
+          echo '<tr><td class="label">Ano de nascimento:</td><td>' . $year . '</td></tr>';
+        }
+      }
+      if (!empty($row['cidade']) || !empty($row['estado'])) {
+        echo '<tr><td class="label">Endereço:</td><td>' . $row['cidade'] . ', ' . $row['estado'] . '</td></tr>';
+      }
+      echo '</table>';
+    } else {
+      echo '<p class="error">Ocorreu um problema ao acessar seu perfil.</p>';
     }
-    if (!empty($row['cidade']) || !empty($row['estado'])) {
-      echo '<tr><td class="label">Endereço:</td><td>' . $row['cidade'] . ', ' . $row['estado'] . '</td></tr>';
-    }
-    if (!empty($row['imagem'])) {
-      echo '<tr><td class="label">Foto de perfil:</td><td><img src="' . MM_UPLOADPATH . $row['imagem'] .
-        '" alt="Foto de perfil" /></td></tr>';
-    }
-    echo '</table>';
-    if (!isset($_GET['id']) || ($id == $_GET['id'])) {
-      echo '<p>Você gostaria de <a href="editprofile.php">atualizar seu perfil</a>?</p>';
-    }
-  } else {
-    echo '<p class="error">Ocorreu um problema ao acessar seu perfil.</p>';
-  }
 
-  mysqli_close($dbc);
-  ?>
-
-
+    mysqli_close($dbc);
+    ?>
+  </section>
 </body>
 
 </html>

401e5cb35badfc10ee0773d31e953e6a52da03d0
Leocaliban <leocaliban@gmail.com>
2019-08-08 07:19:13 -0300

    Aplica responsividade na home page

--- C_07_mismatch/source/php/templates/footer.php
+++ C_07_mismatch/source/php/templates/footer.php
@@ -1,4 +1,5 @@
 <footer class="footer">DESIGNED BY <strong>Leocaliban</strong> &copy; 2019</footer>
+</div>
 </body>
 
 </html>
--- C_07_mismatch/source/php/templates/header.php
+++ C_07_mismatch/source/php/templates/header.php
@@ -8,6 +8,19 @@
     <meta name="description" content="Mismatch - <?php echo $page_title ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/css/style.min.css" />
+
+    <script>
+        window.onload = function() {
+            document.querySelector(".nav__menu__mini").addEventListener("click", function() {
+                if (document.querySelector(".nav__menu ul").style.display == 'flex') {
+                    document.querySelector(".nav__menu ul").style.display = 'none';
+                } else {
+                    document.querySelector(".nav__menu ul").style.display = 'flex';
+                }
+            });
+        };
+    </script>
 </head>
 
 <body>
+    <div class="master-container">

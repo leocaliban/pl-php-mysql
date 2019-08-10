9f5ff8c0ca16a2141907966276cdde1cc8f18ded
Leocaliban <leocaliban@gmail.com>
2019-08-10 04:27:47 -0300

    CAP 09 - Risk Jobs configuração inicial

---
+++ C_09_risk_jobs/gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require('gulp');
+
+const rename = require('gulp-rename');
+const htmlMIN = require('gulp-htmlmin');
+
+const uglifyCSS = require('gulp-uglifycss');
+const cssImport = require('gulp-cssimport');
+
+const notify = require('gulp-notify');
+
+// html
+gulp.task('html', () => {
+    return gulp.src('source/html/*.html')
+        .pipe(htmlMIN({
+            collapseInlineTagWhitespace: true
+        }))
+        .on('error', notify.onError('Erro: <%= error.message %>'))
+        .pipe(gulp.dest('C:\\wamp64\\www\\php\\risk_jobs'));
+});
+
+// image
+gulp.task('image', () => {
+    return gulp.src('source/assets/images/*')
+        .on('error', notify.onError('Erro: <%= error.message %>'))
+        .pipe(gulp.dest('C:\\wamp64\\www\\php\\risk_jobs\\assets\\images'));
+});
+
+// php
+gulp.task('php', () => {
+    return gulp.src('source/php/**')
+        .on('error', notify.onError('Erro: <%= error.message %>'))
+        .pipe(gulp.dest('C:\\wamp64\\www\\php\\risk_jobs'));
+});
+
+// css
+gulp.task('css', () => {
+    return gulp.src('source/assets/css/*.css')
+        .pipe(cssImport())
+        .pipe(uglifyCSS({
+            'maxLineLen': 80,
+            'uglyComments': true
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('C:\\wamp64\\www\\php\\risk_jobs\\assets\\css'));
+});
+
+// watch
+gulp.task('watch', () => {
+    gulp.watch('**/*.html', gulp.task('html'));
+    gulp.watch('**/*.css', gulp.task('css'));
+    gulp.watch('**/*.php', gulp.task('php'));
+    gulp.watch('**/*.jpg', gulp.task('image'));
+    gulp.watch('**/*.png', gulp.task('image'));
+});
+
+
+gulp.task('build', gulp.series(gulp.parallel('html', 'css', 'php', 'image'), gulp.parallel('watch')));
